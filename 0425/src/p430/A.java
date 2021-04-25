3eceb1bf6e9650555228468d0ccd120df383e78b
min0201ji <82108049+min0201ji@users.noreply.github.com>
2021-04-25 13:51:42 +0900

    Add files via upload

--- 0425/src/p430/A.java
+++ 0425/src/p430/A.java
@@ -1,7 +1,19 @@
 package p430;
 
 public class A {
-	RomoteControl field = new RomoteControl() {
+	RemoteControl field = new RemoteControl() {
+
+		@Override
+		public void turnOn() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void turnOff() {
+			// TODO Auto-generated method stub
+			
+		}
 		
-	}
+	};
 }
--- 0425/src/p446/TryCatchFinallyEx.java
+++ 0425/src/p446/TryCatchFinallyEx.java
@@ -4,7 +4,14 @@ public class TryCatchFinallyEx {
 
 	public static void main(String[] args)throws Exception {
 		// TODO Auto-generated method stub
-		Class clazz =  Class.forName("java.lang.String2");
+		
+		
+		try {
+			Class clazz =  Class.forName("java.lang.String"); //java.lang.String1 -> catch로
+			System.out.println("String 클래스가 존재합니다.");
+		}catch(ClassNotFoundException e) {
+			System.out.println("클래스가 존재하지 않습니다.");
+		}
 	}
 
 }
